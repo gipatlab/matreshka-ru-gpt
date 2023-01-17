970527aea40b3d66bcd7863c70bb045f4f443185
gipatlab <gipat.lab@gmail.com>
2023-01-17 12:09:41 -0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -43,11 +43,17 @@ RUN git clone https://github.com/qywu/apex \
 
 RUN pip3 install triton
 
-
 # RUN DS_BUILD_CPU_ADAM=1 DS_BUILD_SPARSE_ATTN=1 pip install --no-dependencies --no-cache-dir deepspeed
-RUN pip install deepspeed
+RUN pip3 install deepspeed
 
 RUN ds_report
+
+RUN git clone https://github.com/sberbank-ai/ru-gpts \
+    && pip3 install transformers \
+    && pip3 install huggingface_hub \
+    && pip3 install timm==0.3.2 \
+    && cp ru-gpts/src_utils/trainer_pt_utils.py /usr/local/lib/python3.8/dist-packages/transformers/trainer_pt_utils.py \
+    && cp ru-gpts/src_utils/_amp_state.py /usr/local/lib/python3.8/dist-packages/apex/amp/_amp_state.py
 
 RUN pip3 install Flask
 
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import deepspeed
+import warnings
+warnings.filterwarnings("ignore")
+import sys
+sys.path.append("ru-gpts/")
+import os
+os.environ["USE_DEEPSPEED"] = "1"
+from src.xl_wrapper import RuGPT3XL
+import torch
+torch.cuda.is_available()
+from src import mpu
 from flask import Flask, request, jsonify
+
+os.environ["MASTER_ADDR"] = "127.0.0.1"
+os.environ["MASTER_PORT"] = "5000"
+
+gpt = RuGPT3XL.from_pretrained("sberbank-ai/rugpt3xl", seq_len=512)
+
+
+def filter_resuls(nr):
+  return [x[:x.find("<|endoftext|>")] for x in nr]
 
 app = Flask(__name__)
 
@@ -6,6 +26,15 @@ app = Flask(__name__)
 def message():
   try:
     message = request.get_json()["message"]
+    res = filter_resuls(gpt.generate(
+      message,
+      do_sample=True,
+      num_return_sequences=5,
+      max_length=50,
+      no_repeat_ngram_size=3,
+      repetition_penalty=2.,
+    ))
+    jsonify({message: res})
   except:
     return jsonify({'error': "Parameter message is required."})
 
